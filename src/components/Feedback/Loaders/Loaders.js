02e22d38bc716983fc4a8b8427a7a9c7a6544f51
Mohamed Gad <gadix1@gmail.com>
2024-10-11 06:19:08 +0000

    Adding Loaders to components/Feedback

---
+++ src/components/Feedback/Loaders/Loaders.js
@@ -0,0 +1,10 @@
+
+function Loaders(selector) {
+    const loaders = document.querySelectorAll(selector);
+    loaders.forEach(loader => {
+        loader.addEventListener('click', () => {
+            alert('You clicked on the loader');
+        });
+    });
+}
+
